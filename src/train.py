be50595b4cb5276635d4c774395b79e1178e5a3d
xiulinyang <xiulin.yang.compling@gmail.com>
2026-01-07 10:22:34 -0500

    update warmup

--- src/train.py
+++ src/train.py
@@ -504,7 +504,7 @@ def parse_args():
     parser.add_argument(
         "--num_warmup_steps",
         type=int,
-        default=40000
+        default=0,
         help="Number of steps for the warmup in the lr scheduler.",
     )
     parser.add_argument(
